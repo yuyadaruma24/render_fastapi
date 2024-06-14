89297e78c9df22f3892d36f9f3a99c4ff412d698
yuyadaruma24 <166777024+yuyadaruma24@users.noreply.github.com>
2024-06-14 12:32:19 +0900

    omikujikinou

--- main.py
+++ main.py
@@ -2,13 +2,32 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+import random  # randomライブラリを追加
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "takoyaki🐙🐙"}
+    return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+
+    return omikuji_list[random.randrange(10)]
