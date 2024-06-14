489c7305bae0178b20df1cc83ac40572a1380edd
yuyadaruma24 <166777024+yuyadaruma24@users.noreply.github.com>
2024-06-14 14:22:10 +0900

    機能の追加

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse #インポート
+
 import random  # randomライブラリを追加
 
 app = FastAPI()
@@ -31,3 +33,22 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.post("/present")
+async def new_naming(present):
+    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
